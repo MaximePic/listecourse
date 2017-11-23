ca2b653140f79ba7dcae495d7f26be0c9c45ba7b
maxim <maxime.pomier@supersoluce.com>
2017-11-23 12:41:17 +0100

    Test route server

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+    res.json({
+        status: 'success',
+        data: {
+            user: { name: 'Jean', age: 45 }
+        }
+    })
 })
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
+
+module.exports = app
