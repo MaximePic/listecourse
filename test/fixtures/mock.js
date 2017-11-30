e5780fc4b915882c7755f0ad4f2b66ae49c816bb
maxime <maxime.pic@hotmail.fr>
2017-11-30 16:16:09 +0100

    data

--- test/fixtures/mock.js
+++ test/fixtures/mock.js
@@ -1,7 +1,13 @@
-const standardArticleList = [
-    { id: 1, name: 'Viande' },
-    { id: 2, name: 'Popcorn' }
-  ]
+const standardArticleList = [{
+    lundi: [
+        { id: 1, name: 'Viande', quantity: 1, status: "nok"},
+        { id: 2, name: 'Popcorn', quantity: 1, status: "nok" }],
+
+    mardi: [
+        { id: 1, name: "pizza", quantity: 1, status: "nok"},
+        { id: 2, name: "Popcorn", quantity: 2, status: "nok"}
+    ]
+}]
 
   module.exports = {
       standardArticleList
