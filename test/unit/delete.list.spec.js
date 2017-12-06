1b366ad965602549a8099e02f31079c30d4f7281
maxim <maxime.pomier@supersoluce.com>
2017-12-06 16:16:12 +0100

    test for delete list UT

--- test/unit/delete.list.spec.js
+++ test/unit/delete.list.spec.js
@@ -4,3 +4,42 @@ Je veux pouvoir supprimer une
 liste de course
 Afin de ne conserver que les listes
 qui me sont utiles */
+
+const app = require('../../server');
+require('chai').should();
+const sinon = require('sinon');
+const request = require('supertest');
+const Components = require('../../app/components/Components');
+const Article = Components.Article;
+
+
+describe('create list', function () {
+    beforeEach(() => {
+        sinon.spy()
+    });
+
+    it('should call /list once', function () {
+        //////////GIVEN///////////
+        let post = sinon.stub(request(app), 'post');
+        post.yields();
+
+        let day = "mardi";
+        let articleList = {
+            deleteList: function (day) {
+                request(app)
+                    .post('/delete')
+                    .send({ day: day })
+            }
+        };
+
+        let deleteListSpy = sinon.spy(articleList, 'deleteList');
+
+        ///////////WHEN///////////
+        articleList.deleteList(day);
+
+        ///////////THEN///////////
+        sinon.assert.called(deleteListSpy)
+        sinon.assert.callCount(deleteListSpy, 1)
+        deleteListSpy.restore();
+    });
+});
