e6a5d93b6aecb85b6090a91a127d05c64a6ebda7
maxim <maxime.pomier@supersoluce.com>
2017-12-06 12:51:00 +0100

    add post delete test

--- test/integration/delete.list.spec.js
+++ test/integration/delete.list.spec.js
@@ -4,3 +4,26 @@ Je veux pouvoir supprimer une
 liste de course
 Afin de ne conserver que les listes
 qui me sont utiles */
+
+const request = require('supertest');
+require('chai').should();
+
+const app = require('../../server');
+const mock = require('./../fixtures/mock');
+const Components = require('../../app/components/Components');
+const courseListFixture = require('./../fixtures/courseList');
+
+//Tests sur l'api /getList/:param
+describe('deleteList', () => {
+
+beforeEach(() => {courseListFixture.up()});
+afterEach(() => {courseListFixture.down()});
+
+it('delete mardi', (done) => {
+    request(app)
+    .post('/delete')
+    .send({ day: 'mardi' })
+    .expect(mock.articleListAfterDeleteList, done)
+})
+
+});
