222f0033cd3172420a48a1cce1686fb981b4bf64
maxim <maxime.pomier@supersoluce.com>
2017-12-06 17:12:03 +0100

    TU for getList and getArticle

--- test/unit/get.article.spec.js
+++ test/unit/get.article.spec.js
@@ -4,3 +4,44 @@ Je veux lister les articles d'une
 liste de course donnée
 Afin de connaitre les articles à
 acheter */
+
+const app = require('../../server');
+require('chai').should();
+const sinon = require('sinon');
+const request = require('supertest');
+const Components = require('../../app/components/Components');
+const Article = Components.Article;
+
+describe('add article', function () {
+    beforeEach(() => {
+        sinon.spy()
+    });
+
+    it('should call /article once', function () {
+        //////////GIVEN///////////
+        let get = sinon.stub(request(app), 'get');
+        get.yields();
+
+        let day = "lundi";
+
+        let article = {
+            getArticle: function (day) {
+                request(app)
+                    .get('/list/' + day)
+            }
+        };
+
+        let getArticleSpy = sinon.spy(article, 'getArticle');
+
+        ///////////WHEN///////////
+        article.getArticle(day);
+
+        ///////////THEN///////////
+        sinon.assert.called(getArticleSpy)
+        sinon.assert.callCount(getArticleSpy, 1)
+        getArticleSpy.restore();
+    });
+
+
+
+});
--- test/unit/get.list.spec.js
+++ test/unit/get.list.spec.js
@@ -4,4 +4,43 @@ Je veux pouvoir afficher les listes
 de course
 Afin de parcourir les listes que j'ai
 créées  */
+const app = require('../../server');
+require('chai').should();
+const sinon = require('sinon');
+const request = require('supertest');
+const Components = require('../../app/components/Components');
+const Article = Components.Article;
+
+describe('add article', function () {
+    beforeEach(() => {
+        sinon.spy()
+    });
+
+    it('should call /article once', function () {
+        //////////GIVEN///////////
+        let get = sinon.stub(request(app), 'get');
+        get.yields();
+
+        let list = {
+            getList: function () {
+                request(app)
+                    .get('/list')
+            }
+        };
+
+        let getListSpy = sinon.spy(list, 'getList');
+
+        ///////////WHEN///////////
+        list.getList();
+
+        ///////////THEN///////////
+        sinon.assert.called(getListSpy)
+        sinon.assert.callCount(getListSpy, 1)
+        getListSpy.restore();
+    });
+
+
+});
+
+
 
