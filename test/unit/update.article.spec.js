7f800ec820b37833e88b816a43e88b579610eb74
maxim <maxime.pomier@supersoluce.com>
2017-12-06 17:05:41 +0100

    TU update

--- test/unit/update.article.spec.js
+++ test/unit/update.article.spec.js
@@ -4,3 +4,47 @@ Je veux flaguer des articles de ma
 liste de course comme OK
 Afin de savoir quels articles j'ai
 déjà acheté */
+
+const app = require('../../server');
+require('chai').should();
+const sinon = require('sinon');
+const request = require('supertest');
+const Components = require('../../app/components/Components');
+const Article = Components.Article;
+const generateID = require('./../fixtures/generateUuid');
+
+
+describe('add article', function () {
+    beforeEach(() => {
+        sinon.spy()
+    });
+
+    it('should call /article once', function () {
+        //////////GIVEN///////////
+        let post = sinon.stub(request(app), 'post');
+        post.yields();
+
+        let day = "lundi";
+        let id = generateID.generateUuid();
+
+        let articles = {
+            updateArticle: function (day, id) {
+                request(app)
+                    .post('/changeArticleStatus')
+                    .send({day, id})
+            }
+        };
+
+        let updateArticleSpy = sinon.spy(articles, 'updateArticle');
+
+        ///////////WHEN///////////
+        articles.updateArticle(day, id);
+
+        ///////////THEN///////////
+        sinon.assert.called(updateArticleSpy)
+        sinon.assert.callCount(updateArticleSpy, 1)
+        updateArticleSpy.restore();
+    });
+
+
+});
